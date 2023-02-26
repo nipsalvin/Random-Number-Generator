ee8c6688e600482c019f8a7ef12844f4aebd3c8c
nipsalvin <amwaniki.am@gmail.com>
2023-02-26 19:28:30 +0300

    upgrade

--- rand-int.py
+++ rand-int.py
@@ -1,3 +1,7 @@
-import random
-num = random.randint(1,10)
+# import random
+# num = random.randint(1,10)
+# print(f"The number is {num}")
+
+from random import randint
+num = randint(1,10)
 print(f"The number is {num}")
